ec1e5356a4192b56ad559be98d9f53dfef32fb98
Shalhriyor1408 <sohidjonovshahriyor1408@gmail.com>
2023-02-06 11:24:41 +0500

    Bismillahir rohmanir rohim. My first commit.

---
+++ src/main/java/com/company/milliyuniversity/NationalUniversityApplication.java
@@ -0,0 +1,13 @@
+package com.company.milliyuniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalUniversityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NationalUniversityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/milliyuniversity/MilliyUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.milliyuniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilliyUniversityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
