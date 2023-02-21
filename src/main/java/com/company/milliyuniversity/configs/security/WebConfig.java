114112faa1390575c5e05c030dc80e043ea1e807
Shalhriyor1408 <sohidjonovshahriyor1408@gmail.com>
2023-02-21 14:52:16 +0500

    Bismillahir rohmanir rohim. My fifth commit.

--- src/main/java/com/company/milliyuniversity/configs/security/WebConfig.java
+++ src/main/java/com/company/milliyuniversity/configs/security/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://milliy-university.netlify.app")
+                .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .exposedHeaders("*")
